b923eed620b1d9366486f85a13a2c487c224845a
Bakhumhlea <Tatuu@bakhumhlea.com>
2017-12-18 11:50:19 -0800

    6-assignment

--- src/components/Album.js
+++ src/components/Album.js
@@ -14,7 +14,7 @@ class Album extends Component {
     return (
       <section className="album">
         <section id="album-info">
-          <img id="album-cover-art" src={this.state.album.albumCover}/>
+          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
           <div className="album-details">
             <h1 id="album-title">{this.state.album.title}</h1>
             <h2 className="artist">{this.state.album.artist}</h2>
@@ -28,6 +28,23 @@ class Album extends Component {
             <col id="song-duration-column" />
           </colgroup>
           <tbody>
+            {this.state.album.songs.map( (song ,index) =>
+              <tr key={index}>
+                <td id="song-number-column">
+                  <button>
+                    <span className="song-number">{index+1}</span>
+                    <span className="ion-play"></span>
+                    <span className="ion-pause"></span>
+                  </button>
+                </td>
+                <td id="song-title-column">
+                  {song.title}
+                </td>
+                <td id="song-duration-column">
+                  {song.duration}
+                </td>
+              </tr>
+          )}
           </tbody>
         </table>
       </section>
